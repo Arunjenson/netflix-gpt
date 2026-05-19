3947a4ea61abc21d9d1f8c105dda05d200540e12
Arunjenson-SurveySparrow <arun.jenson@surveysparrow.com>
2026-05-19 22:09:22 +0530

    feat: enhance Login component with styling and sign-in form

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import { Logo } from "./icons/logo";
 
 const Header = () => {
   return (
-    <div className="absolute top-0 px-8 py-2 bg-linear-to-b from-black">
+    <div className="absolute top-0 px-8 py-3 bg-linear-to-b from-black">
       <div className="">
         <Logo />
       </div>
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,10 +2,21 @@ import Header from "./Header"
 
 const Login = () => {
   return (
-    <div>
+    <div className="relative h-screen w-screen">
       <Header/>
-      <div className="size-full">
+      <div className="size-full absolute top-0 left-0 right-0 bottom-0 -z-10">
         <img src="https://assets.nflxext.com/ffe/siteui/vlv3/77c412a9-62ea-48a0-a5ee-466e11e851d5/web/IN-en-20260511-TRIFECTA-perspective_f0af4f75-4cc5-42bd-b0c5-2b65b8b50e03_large.jpg" alt="" />
+      </div>
+      <div className="z-10 flex items-center justify-center size-full max-w-md mx-auto">
+        
+        <div className="bg-black/80 p-8 rounded-md">
+          <h1 className="text-4xl font-bold text-white mb-4">Sign in</h1>
+          <input type="text" placeholder="Email or phone number" className="w-full p-3 mb-4 rounded-md bg-gray-800 text-white focus:outline-none focus:ring-2 focus:ring-red-600"/>
+          <input type="password" placeholder="Password" className="w-full p-3 mb-4 rounded-md bg-gray-800 text-white focus:outline-none focus:ring-2 focus:ring-red-600"/>
+          <button className="bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 w-full rounded">
+            Sign In
+          </button>
+        </div>
       </div>
     </div>
   )
