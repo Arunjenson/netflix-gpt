1a0b6425f0ba7d65971c8f4bab68d1a4d64bd3f2
Arunjenson-SurveySparrow <arun.jenson@surveysparrow.com>
2026-05-19 21:30:18 +0530

    feat: implement main application structure with Body, Header, Login, and Browse components

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
+import Body from './components/Body'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className='text-5xl'>
-     hello world
-    </div>
+    <Body />
   )
 }
 
