5405bc1c66348ae85b63c46e569f261131bbf582
Arunjenson <jensonarun8@gmail.com>
2026-06-13 20:13:33 +0530

    feat: add form validation logic and update Login component

---
+++ src/utils/validate.js
@@ -0,0 +1,18 @@
+export const validateData = (email,password,name) => {
+    const isEmailValid = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email);
+    const isPasswordValid = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/.test(password);
+    const isNameValid = /^[a-zA-Z\s]{2,}$/i.test(name);
+
+    if (!isNameValid) {
+        return "Name is not valid";
+    }
+
+    if (!isEmailValid) {
+        return "Email is not valid";
+    }
+    if (!isPasswordValid) {
+        return "Password is not valid";
+    }
+    return null;
+
+}
