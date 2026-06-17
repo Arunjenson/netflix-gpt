9752e2da6308a6574726bdc7f016b446372af22a
Arunjenson <jensonarun8@gmail.com>
2026-06-17 20:20:58 +0530

    feat: implement movie fetching and state management for now playing movies

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,11 +1,33 @@
-import Header from "./Header"
+import { useEffect } from "react";
+import { API_OPTIONS } from "../utils/constants";
+import Header from "./Header";
+import { useDispatch } from "react-redux";
+import { addNowPlayingMovies } from "../utils/movieSlice";
 
 const Browse = () => {
+  const dispatch = useDispatch();
+  const getNowPlayingMovies = async () => {
+    try {
+      const response = await fetch(
+        "https://api.themoviedb.org/3/movie/now_playing?npmpage=1",
+        API_OPTIONS,
+      );
+      const data = await response.json();
+      console.log("Now Playing Movies:", data.results);
+      dispatch(addNowPlayingMovies(data.results));
+    } catch (error) {
+      console.error("Error fetching now playing movies:", error);
+    }
+  };
+  useEffect(() => {
+    getNowPlayingMovies();
+  }, []);
   return (
     <>
-    <Header />
-    <div>Browse</div></>
-  )
-}
+      <Header />
+      <div>Browse</div>
+    </>
+  );
+};
 
-export default Browse
+export default Browse;
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,9 @@
 export const DEFAULT_PROFILE_IMAGE = "https://avatars.githubusercontent.com/u/116062066?v=4";
+
+export const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4NDRlMmUzODc0ZjNlOTY1OTg2ZmM2YWI0Y2QwYjM3OSIsIm5iZiI6MTc4MTcwNjQ4MS40NTYsInN1YiI6IjZhMzJhZWYxMjVkMDY3YzZhOGQ5ZjI1MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.fssXa7OGwEi1OiJPz0TFuWchiwb8czOkbWEXYdK93sU'
+  }
+};
